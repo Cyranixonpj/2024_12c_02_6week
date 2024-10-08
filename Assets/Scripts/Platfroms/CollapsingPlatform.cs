854dbfe50b223296714577f3b14e9a77060ad3f7
Cyranixonpj <163138757+Cyranixonpj@users.noreply.github.com>
2024-10-08 17:40:00 +0200

    CollapsingPlatformPrefab

---
+++ Assets/Scripts/Platfroms/CollapsingPlatform.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CollapsingPlatform : MonoBehaviour
+{
+    [SerializeField] private float closeAgainCooldown;
+    [SerializeField] private float openDelay;
+    [SerializeField] private BoxCollider2D bxCol;
+    private bool _isOpening = false;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(_isOpening)
+            return;
+        if (other.transform.tag=="Player")
+        {
+            _isOpening = true;
+            StartCoroutine(Opening());
+        }
+        
+    }
+    
+    private IEnumerator Opening()
+    {
+        while (true)
+        {
+            _isOpening = true;
+            yield return new WaitForSeconds(openDelay);
+            bxCol.enabled = false;
+            yield return new WaitForSeconds(closeAgainCooldown);
+            bxCol.enabled = true;
+        }
+        
+
+    }
+}
