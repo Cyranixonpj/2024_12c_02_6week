a5dec9faecc5c655156717ab247f966bf72ad22c
Cyranixonpj <163138757+Cyranixonpj@users.noreply.github.com>
2024-10-08 15:49:38 +0200

    Totem Right and CannonballRight
    Totem ktory strzela w prawo i canonball lecący w prawo

---
+++ Assets/Scripts/Obstacles/TotemShooting.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotemShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject _cannonballPrefab;
+    [SerializeField] private float _cooldown;
+
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+    
+    private IEnumerator Shoot()
+    {
+        while (true)
+        {
+            Instantiate(_cannonballPrefab, transform.position, transform.rotation);
+            yield return new WaitForSeconds(_cooldown);
+            
+        }
+    }
+
+    
+}
