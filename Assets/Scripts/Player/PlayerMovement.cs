b93fe3e8cc79ef808289dd8f8a6d03259bd268a6
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-10-12 12:31:58 +0100

    Basic jumping mechanics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,10 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private float _xInput;
+    private bool _isGrounded;
+    private bool _performedJump;
     
+    [SerializeField] private float JumpForce = 5;
     [SerializeField] private float Speed = 5;
 
 
@@ -17,11 +20,32 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         _xInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _performedJump = true;
+        }
     }
+    
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        _isGrounded = false;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        _isGrounded = true;
+    }
+
     
     
     private void FixedUpdate()
     {
         _rb.velocity = new Vector2(_xInput * Speed, _rb.velocity.y);
+        if (_performedJump)
+        {
+            _performedJump = false;
+            _rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
