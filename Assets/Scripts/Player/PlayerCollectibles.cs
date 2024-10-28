051d3a1cbdca13dda1be210433257fd5b4fdfbe2
Wiktoria Przybylak <s28750@pjwstk.edu.pl>
2024-10-28 14:23:14 +0100

    HUD silver coins/ diamonds

--- Assets/Scripts/Player/PlayerCollectibles.cs
+++ Assets/Scripts/Player/PlayerCollectibles.cs
@@ -6,11 +6,20 @@ public class PlayerCollectibles : MonoBehaviour
 {
     private int _goldCoinCounter;
     public TMP_Text _goldCoinText;
+    private int _silverCoinCounter;
+    public TMP_Text _silverCoinText;
+    private int _diamondCounter;
+    public TMP_Text _diamondText;
+    
 
     private void Awake()
     {
         _goldCoinCounter = 0;
+        _silverCoinCounter = 0;
+        _diamondCounter = 0;
         _goldCoinText.text = _goldCoinCounter.ToString();
+        _silverCoinText.text = _silverCoinCounter.ToString();
+        _diamondText.text = _diamondCounter.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -19,6 +28,14 @@ public class PlayerCollectibles : MonoBehaviour
         {
             AddGoldCoin();
         }
+        if (other.gameObject.CompareTag("Silver coins"))
+        {
+            AddSilverCoin();
+        }
+        if (other.gameObject.CompareTag("Diamonds"))
+        {
+            AddDiamond();
+        }
     }
 
 
@@ -26,5 +43,15 @@ public class PlayerCollectibles : MonoBehaviour
     {
         _goldCoinCounter += 1;
         _goldCoinText.text = _goldCoinCounter.ToString();
+    }
+    private void AddSilverCoin()
+    {
+        _silverCoinCounter += 1;
+        _silverCoinText.text = _silverCoinCounter.ToString();
+    }
+    private void AddDiamond()
+    {
+        _diamondCounter += 1;
+        _diamondText.text = _diamondCounter.ToString();
     }
 }
