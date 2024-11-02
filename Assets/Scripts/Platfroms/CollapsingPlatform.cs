671a3ca8b3427e9c304d8e1e61fd0a5a704f067f
Cyranixonpj <163138757+Cyranixonpj@users.noreply.github.com>
2024-11-02 15:44:50 +0100

    Collapsing Platform finished
    Tak robilem 2h platforme a co

--- Assets/Scripts/Platfroms/CollapsingPlatform.cs
+++ Assets/Scripts/Platfroms/CollapsingPlatform.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -7,30 +8,66 @@ namespace Platfroms
     {
         [SerializeField] private float closeAgainCooldown;
         [SerializeField] private float openDelay;
-        [SerializeField] private BoxCollider2D bxCol;
+        
+        [SerializeField] private Animator _part1Animator;
+        [SerializeField]private Animator _part2Animator;
+        [SerializeField]private Collider2D _part1Collider;
+        [SerializeField]private Collider2D _part2Collider;
+        
+        
+        // private Animator _animator;
         private bool _isOpening = false;
+        private bool _playerOnPlatform = false;
+
+        private void Awake()
+        {
+            
+        }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if(_isOpening)
-                return;
-            if (other.transform.tag=="Player")
+            if (other.transform.CompareTag("Player") && !_isOpening)
             {
+                _playerOnPlatform = true;
                 _isOpening = true;
                 StartCoroutine(Opening());
             }
-        
         }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.transform.CompareTag("Player"))
+            {
+                _playerOnPlatform = false;
+            }
+        }
+
         private IEnumerator Opening()
         {
-            while (true)
-            {
-                _isOpening = true;
-                yield return new WaitForSeconds(openDelay);
-                bxCol.enabled = false;
-                yield return new WaitForSeconds(closeAgainCooldown);
-                bxCol.enabled = true;
-            }
+           _part1Animator.SetTrigger("Open");
+           _part2Animator.SetTrigger("Open");
+
+           yield return new WaitForSeconds(openDelay);
+
+           if (_playerOnPlatform)
+           {
+               _part1Collider.enabled = false;
+               _part2Collider.enabled = false;
+               yield return new WaitForSeconds(closeAgainCooldown);
+               // yield return new WaitForSeconds(closeAgainCooldown);
+               _part1Collider.enabled = true;
+               _part2Collider.enabled = true;
+              
+           }
+          
+           
+           _isOpening = false;
+           
+                // yield return new WaitForSeconds(openDelay);
+                // bxCol.enabled = false;
+                // yield return new WaitForSeconds(closeAgainCooldown);
+                // bxCol.enabled = true;
+            
         }
     }
 }
