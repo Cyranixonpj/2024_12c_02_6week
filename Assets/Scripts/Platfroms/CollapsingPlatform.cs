a583a73920c4f5b525966a0cad63ee4260fa723c
Cyranixonpj <163138757+Cyranixonpj@users.noreply.github.com>
2024-11-04 22:24:04 +0100

    Level 0 and collapsing platform fix

--- Assets/Scripts/Platfroms/CollapsingPlatform.cs
+++ Assets/Scripts/Platfroms/CollapsingPlatform.cs
@@ -8,11 +8,13 @@ namespace Platfroms
     {
         [SerializeField] private float closeAgainCooldown;
         [SerializeField] private float openDelay;
+        [SerializeField] private BoxCollider2D bxCol;
         
         [SerializeField] private Animator _part1Animator;
         [SerializeField]private Animator _part2Animator;
-        [SerializeField]private Collider2D _part1Collider;
-        [SerializeField]private Collider2D _part2Collider;
+        // [SerializeField]private Collider2D _part1Collider;
+        // [SerializeField]private Collider2D _part2Collider;
+        
         
         
         // private Animator _animator;
@@ -21,6 +23,7 @@ namespace Platfroms
 
         private void Awake()
         {
+            
             
         }
 
@@ -44,32 +47,61 @@ namespace Platfroms
 
         private IEnumerator Opening()
         {
-            float animationTime = closeAgainCooldown;
-            float part1Speed = _part1Animator.speed;
-            float part2Speed = _part2Animator.speed;
-           _part1Animator.SetTrigger("Open");
-           _part2Animator.SetTrigger("Open");
+           //  float animationTime = closeAgainCooldown;
+           //  float part1Speed = _part1Animator.speed;
+           //  float part2Speed = _part2Animator.speed;
+           // _part1Animator.SetTrigger("Open");
+           // _part2Animator.SetTrigger("Open");
+           //
+           // _part1Animator.speed = 1f/animationTime;
+           // _part2Animator.speed = 1f/animationTime;
+           //
+           //
+           // if (_playerOnPlatform)
+           // {
+           //     yield return new WaitForSeconds(openDelay);
+           //     _part1Collider.enabled = false;
+           //     _part2Collider.enabled = false;
+           //     
+           //     // _collider2D.enabled = false;
+           //
+           //     yield return new WaitForSeconds(closeAgainCooldown);
+           //     
+           //     // _collider2D.enabled = true;
+           //
+           //     _part1Collider.enabled = true;
+           //     _part2Collider.enabled = true;
+           // }
+           //  _part1Animator.speed = part1Speed;
+           //  _part2Animator.speed = part2Speed;
+           //
+           // _isOpening = false;
+           
+           
+           
+           float animationTime = closeAgainCooldown;
+           float part1Speed = _part1Animator.speed;
+           float part2Speed = _part2Animator.speed;
+           
+           _part1Animator.speed = part1Speed;
+           _part2Animator.speed = part2Speed;
            
            _part1Animator.speed = 1f/animationTime;
            _part2Animator.speed = 1f/animationTime;
-            
-           
-           yield return new WaitForSeconds(openDelay);
-
            if (_playerOnPlatform)
            {
-               _part1Collider.enabled = false;
-               _part2Collider.enabled = false;
-
+               yield return new WaitForSeconds(openDelay);
+               _part1Animator.SetTrigger("Open");
+               _part2Animator.SetTrigger("Open");
+               bxCol.enabled = false;
                yield return new WaitForSeconds(closeAgainCooldown);
-
-               _part1Collider.enabled = true;
-               _part2Collider.enabled = true;
+               bxCol.enabled = true;
+               
+               
            }
-            _part1Animator.speed = part1Speed;
-            _part2Animator.speed = part2Speed;
-
            _isOpening = false;
+               
+              
            
                 
             
