c6c9f95bc2533d5dba0b7e43c0fd641a4d2e5ecf
jaho123i <60041198+jaho123i@users.noreply.github.com>
2024-11-08 19:45:47 +0100

    Chest Script added

---
+++ Assets/Scripts/Obstacles/Chest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private Collider2D _triggerCollider2D;
+    private Animator _animator;
+
+    private void Awake()
+    {
+
+        _animator = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetTrigger("IsOpen");
+            _triggerCollider2D.enabled = false;
+        }
+    }
+}
