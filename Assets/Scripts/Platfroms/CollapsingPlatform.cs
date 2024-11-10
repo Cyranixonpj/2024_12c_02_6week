50e196e4264aee267134fa47c327fc527f79df6d
Aleksander Cyran <s27730@pjwstk.edu.pl>
2024-11-11 00:53:39 +0100

    Clouds

--- Assets/Scripts/Platfroms/CollapsingPlatform.cs
+++ Assets/Scripts/Platfroms/CollapsingPlatform.cs
@@ -12,20 +12,9 @@ namespace Platfroms
         
         [SerializeField] private Animator _part1Animator;
         [SerializeField]private Animator _part2Animator;
-        // [SerializeField]private Collider2D _part1Collider;
-        // [SerializeField]private Collider2D _part2Collider;
         
-        
-        
-        // private Animator _animator;
         private bool _isOpening = false;
         private bool _playerOnPlatform = false;
-
-        private void Awake()
-        {
-            
-            
-        }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
@@ -47,37 +36,6 @@ namespace Platfroms
 
         private IEnumerator Opening()
         {
-           //  float animationTime = closeAgainCooldown;
-           //  float part1Speed = _part1Animator.speed;
-           //  float part2Speed = _part2Animator.speed;
-           // _part1Animator.SetTrigger("Open");
-           // _part2Animator.SetTrigger("Open");
-           //
-           // _part1Animator.speed = 1f/animationTime;
-           // _part2Animator.speed = 1f/animationTime;
-           //
-           //
-           // if (_playerOnPlatform)
-           // {
-           //     yield return new WaitForSeconds(openDelay);
-           //     _part1Collider.enabled = false;
-           //     _part2Collider.enabled = false;
-           //     
-           //     // _collider2D.enabled = false;
-           //
-           //     yield return new WaitForSeconds(closeAgainCooldown);
-           //     
-           //     // _collider2D.enabled = true;
-           //
-           //     _part1Collider.enabled = true;
-           //     _part2Collider.enabled = true;
-           // }
-           //  _part1Animator.speed = part1Speed;
-           //  _part2Animator.speed = part2Speed;
-           //
-           // _isOpening = false;
-           
-           
            
            float animationTime = closeAgainCooldown;
            float part1Speed = _part1Animator.speed;
@@ -100,10 +58,6 @@ namespace Platfroms
                
            }
            _isOpening = false;
-               
-              
-           
-                
             
         }
     }
